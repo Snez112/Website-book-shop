5ba7a2fa5615529c72407d063215c058e93b9793
NA <rongrangsun25@gmail.com>
2023-10-16 01:13:35 +0700

    add guard

---
+++ Book_shop_FE_user/src/app/guard/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { SessionService } from '../services/session.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  
+  constructor(public session: SessionService, private _router: Router) { }
+
+  canActivate(): boolean {
+    if (this.session.getUser()) {
+      return true;
+    } else {
+      this._router.navigate(['/sign-form'])
+      return false;
+    }
+  }
+  
+}
